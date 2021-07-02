654425b898fff167bea5b31772f80790669b51d3
ambosing <ambosing_@naver.com>
2021-07-02 19:34:05 +0900

    EurekaAlarm add

---
+++ src/main/java/com/example/eurekaalarm/EurekaAlarmApplication.java
@@ -0,0 +1,13 @@
+package com.example.eurekaalarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaAlarmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaAlarmApplication.class, args);
+    }
+
+}
